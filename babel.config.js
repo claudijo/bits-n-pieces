dbac6c29417d729aeabd8d80b888f642fdd43e5c
claudijo <claudijo.borovic@gmail.com>
2020-01-13 11:44:57 +0300

    Update Babel config

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,7 @@ module.exports = function (api) {
     // Override in `.babelrc` in sub folder if needed.
     // Browserslist’s default browsers (> 0.5%, last 2 versions, Firefox ESR,
     // not dead).
-    ["@babel/env", { targets: 'defaults' } ],
+    ['@babel/env', { targets: 'defaults' }],
   ];
 
   const plugins = [
@@ -20,8 +20,8 @@ module.exports = function (api) {
     // Together with cli flag `--root-mode upward`, the following is required to
     // pick up `.babelrc` files in each individual package sub folder
     babelrcRoots: [
-      ".",
-      "packages/*",
+      '.',
+      'packages/*',
     ],
   };
 };
