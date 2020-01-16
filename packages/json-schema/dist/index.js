18cc0553b2c55f19044efc85d3761c3e808542c3
claudijo <claudijo.borovic@gmail.com>
2020-01-16 18:18:03 +0300

    Add missing file

---
+++ packages/json-schema/dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "errorDetails", {
+  enumerable: true,
+  get: function () {
+    return _errors.errorDetails;
+  }
+});
+Object.defineProperty(exports, "sanitizeErrors", {
+  enumerable: true,
+  get: function () {
+    return _errors.sanitizeErrors;
+  }
+});
+
+var _errors = require("./errors");
